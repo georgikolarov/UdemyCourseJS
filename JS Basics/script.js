5d1005890a1bdc37128d8527217f913d54a7a75e
Georgi Kolarov <gkolarov@mw-medianetworks.eu>
2019-02-22 17:27:12 +0200

    Next tutorial Operators

--- JS Basics/script.js
+++ JS Basics/script.js
@@ -1,4 +1,4 @@
-var firstName = 'John';
+/*var firstName = 'John';
 console.log(firstName);
 
 var lastName = 'Smith';
@@ -12,6 +12,47 @@ console.log(job);
 
 job = 'Teacher';
 console.log(job);
+*/
 
+// Variable naming rules
+/******************
 var _3year = 3;
 var johnMark = "John and Mark";
+var if = 23;
+*/
+
+/*******************
+ * Variable mutation and type coercion
+ */
+/*
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name? ');
+console.log(firstName + ' ' + lastName);
+*/
+
+/*****************
+ * Basic operators
+ */
+
+
+
+
+
