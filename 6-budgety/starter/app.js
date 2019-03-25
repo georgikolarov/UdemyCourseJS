247072bc3718871082524d5e3c5f3f7c8e8cccda
Georgi Kolarov <gkolarov@mw-medianetworks.eu>
2019-03-25 17:59:21 +0200

    Ready mistake

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -9,14 +9,22 @@ var budgetController = (function () {
 
 // UI CONTROLLER
 var UIController = (function () {
-
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: '.add__btn'
+    }
     return {
         getInput: function () {
             return {
-                type: document.querySelector('.add__type').value, // Will be either inc or exp
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             };
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
         }
     };
 })();
@@ -25,8 +33,10 @@ var UIController = (function () {
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
     var ctrlAddItem = function () {
         // 1. Get the field input data
+
         var input = UICtrl.getInput();
         console.log(input);
 
@@ -39,7 +49,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 5. Display the budget on the UI
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
 
     document.addEventListener('keypress', function (event) {
@@ -48,4 +58,4 @@ var controller = (function (budgetCtrl, UICtrl) {
             ctrlAddItem();
         }
     });
-})(); //invoke the function
+})(budgetController, UIController); //invoke the function
