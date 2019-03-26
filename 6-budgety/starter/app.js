f9f34df4cb2d84109e575cc0f8293e69e5ae6191
Georgi Kolarov <gkolarov@mw-medianetworks.eu>
2019-03-26 16:17:25 +0200

    tutorial number 82 finished

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,10 +1,66 @@
-
-
+// 'NEW' keyword creates a new empty object
+// for function constructor we use Capital letter
 // BUDGET CONTROLLER
 var budgetController = (function () {
 
+    var Expense = function (id, description, value) { // function constructor
+        this.id = id;
+        this.description = description;
+        this.value = valu;
+    }
 
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var allExpenses = [];
+    var allIncomes = [];
+    var totalExpenses = 0;
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            //[1,2,3,4,5], next ID = 6
+            //[1 2 3 4 6 8], next ID = 9
+            // ID = last ID + 1 
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            //Return the new element
+            return newItem;
+        },
+        testing: function () {
+            console.log(data);
+        }
+    };
 })();
 
 // UI CONTROLLER
@@ -33,14 +89,30 @@ var UIController = (function () {
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
+
+
+        document.addEventListener('keypress', function (event) {
+
+            if (event.keyCode === 13 || event.which === 13) { // some brosers don't use the keyCode property
+                ctrlAddItem();
+            }
+        });
+    };
+
+
+
     var ctrlAddItem = function () {
+        var input, newItem;
         // 1. Get the field input data
 
-        var input = UICtrl.getInput();
-        console.log(input);
+        input = UICtrl.getInput();
 
         // 2. Add the item to the buget controller 
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
@@ -49,13 +121,13 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 5. Display the budget on the UI
     }
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
-
-
-    document.addEventListener('keypress', function (event) {
-
-        if (event.keyCode === 13 || event.which === 13) { // some brosers don't use the keyCode property
-            ctrlAddItem();
+    return {
+        init: function () {
+            console.log('Application has started.');
+            setupEventListeners();
         }
-    });
+    };
+
 })(budgetController, UIController); //invoke the function
+
+controller.init();
